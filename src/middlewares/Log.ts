08f78f840f66a4a14a2bae743db848f1dc4216df
Lê Thanh Tân <tanlethanh8266@gmail.com>
2022-11-11 21:59:42 +0700

    Fix: fix log bug

--- src/middlewares/Log.ts
+++ src/middlewares/Log.ts
@@ -3,6 +3,7 @@
  */
 
 import * as fs from 'fs';
+import { FileHandle } from 'fs/promises';
 import * as path from 'path';
 
 class Log {
@@ -23,17 +24,17 @@ class Log {
 	}
 
 	// Adds INFO prefix string to the log string
-	public info (_string: string): void {
+	public info(_string: string): void {
 		this.addLog('INFO', _string);
 	}
 
 	// Adds WARN prefix string to the log string
-	public warn (_string: string): void {
+	public warn(_string: string): void {
 		this.addLog('WARN', _string);
 	}
 
 	// Adds ERROR prefix string to the log string
-	public error (_string: string): void {
+	public error(_string: string): void {
 		// Line break and show the first line
 		console.log('\x1b[31m%s\x1b[0m', '[ERROR] :: ' + _string.split(/r?\n/)[0]);
 
@@ -41,38 +42,34 @@ class Log {
 	}
 
 	// Adds the custom prefix string to the log string
-	public custom (_filename: string, _string: string): void {
+	public custom(_filename: string, _string: string): void {
 		this.addLog(_filename, _string);
+	}
+
+
+	private resetTime() {
+		this.today = new Date()
+		let _dateString = `${this.today.getFullYear()}-${(this.today.getMonth() + 1)}-${this.today.getDate()}`;
+		let _timeString = `${this.today.getHours()}:${this.today.getMinutes()}:${this.today.getSeconds()}`;
+
+		this.baseDir = path.join(__dirname, '../../.logs/');
+
+		this.fileName = `${_dateString}.log`;
+		this.linePrefix = `[${_dateString} ${_timeString}]`;
 	}
 
 	/**
 	 * Creates the file if does not exist, and
 	 * append the log kind & string into the file.
 	 */
-	private addLog (_kind: string, _string: string): void {
-		const _that = this;
-		_kind = _kind.toUpperCase();
+	private addLog(_kind: string, _string: string): void {
+		this.resetTime()
+		const fileName = `${this.baseDir}${this.fileName}`
 
-		fs.open(`${_that.baseDir}${_that.fileName}`, 'a', (_err, _fileDescriptor) => {
-			if (!_err && _fileDescriptor) {
-				// Append to file and close it
-				fs.appendFile(_fileDescriptor, `${_that.linePrefix} [${_kind}] ${_string}\n`, (_err) => {
-					if (! _err) {
-						fs.close(_fileDescriptor, (_err) => {
-							if (! _err) {
-								return true;
-							} else {
-								return console.log('\x1b[31m%s\x1b[0m', 'Error closing log file that was being appended');
-							}
-						});
-					} else {
-						return console.log('\x1b[31m%s\x1b[0m', 'Error appending to the log file');
-					}
-				});
-			} else {
-				return console.log('\x1b[31m%s\x1b[0m', 'Error cloudn\'t open the log file for appending');
-			}
-		});
+		fs.promises.open(fileName, 'a')
+			.then((fileHandle: FileHandle)=>{
+				fileHandle.appendFile(`${this.linePrefix} [${_kind.toUpperCase()}] ${_string}\n`)
+			})
 	}
 
 	/**
@@ -80,7 +77,7 @@ class Log {
 	 *
 	 * Note: 'X' is defined in .env file
 	 */
-	public clean (): void {
+	public clean(): void {
 		//
 	}
 }
