cbe1721db63235d34544b4b9dcbecd72a5a95a65
Lê Thanh Tân <tanlethanh8266@gmail.com>
2022-11-11 19:22:35 +0700

    Update: add reservation schema, update .gitignore

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Express_1 = __importDefault(require("./providers/Express"));
-console.log("Hello App");
-Express_1.default.initApp();
