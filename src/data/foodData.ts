2ab2876641a4c5dbbdf914bf4b20e9353a21a055
Atomic-duck <ngtrinhthuc@gmail.com>
2022-11-25 20:10:43 +0700

    insert food and user data

---
+++ src/data/foodData.ts
@@ -0,0 +1,88 @@
+const imgPath: string = "../public/images/foods";
+const categories: string[] = ["Món cá", "Món chè", "Món khác"]
+export const foodDatas = [
+   {
+      name: "Bao tử cá",
+      price: 129000,
+      category: categories[0],
+      img: imgPath + 'f-1.jpg',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+   },
+   {
+      name: "Cá lăng thêm",
+      price: 259000,
+      category: categories[0],
+      img: imgPath + 'f-2.png',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+   },
+   {
+      name: "Chả cá đế vương 1",
+      price: 129000,
+      category: categories[0],
+      img: imgPath + 'f-3.jpg',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+   },
+   {
+      name: "Chả cá đế vương 2",
+      price: 179000,
+      category: categories[0],
+      img: imgPath + 'f-4.png',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+   },
+   {
+      name: "Chảo cá nhỏ",
+      price: 89000,
+      category: categories[0],
+      img: imgPath + 'f5.png',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+   },
+   {
+      name: "Chè bưởi",
+      price: 29000,
+      category: categories[1],
+      img: imgPath + 'f6.png',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+   },
+   {
+      name: "Chè đế vương",
+      price: 29000,
+      category: categories[1],
+      img: imgPath + 'f7.jpg',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+   },
+   {
+      name: "Dưa xào lòng cá",
+      price: 69000,
+      category: categories[0],
+      img: imgPath + 'f8.jpg',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+   },
+   {
+      name: "Khoai tây lắc",
+      price: 69000,
+      category: categories[2],
+      img: imgPath + 'f9.png',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+   },
+   {
+      name: "Lẩu cá lăng",
+      price: 439000,
+      category: categories[0],
+      img: imgPath + 'f10.png',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+   },
+   {
+      name: "Lẩu đầu cá",
+      price: 189000,
+      category: categories[0],
+      img: imgPath + 'f11.jpg',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+   },
+   {
+      name: "Rau ăn kèm",
+      price: 69000,
+      category: categories[2],
+      img: imgPath + 'f12.jpg',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
+   },
+];
