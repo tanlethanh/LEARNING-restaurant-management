571c1d228781e8cc6d95c15f8e709d3b34389652
tpphuhungbp <tpphuhungbp2@gmail.com>
2022-11-30 21:08:15 +0700

    add controller

---
+++ public/js/homepage-reservation.js
@@ -0,0 +1,5 @@
+document.getElementById("make-reservation").addEventListener("click", reservationButtonOnClick);
+
+function reservationButtonOnClick() {
+    console.log('Get reservation date')
+}
--- src/routes/webRoute/indexRoute.ts
+++ src/routes/webRoute/indexRoute.ts
@@ -1,10 +1,10 @@
 import express, { Router } from 'express';
-
+import HomepageController from '../../controllers/HomepageController'
 const indexRoute: Router = express.Router()
 indexRoute
-    .get("/")
+    .get("/", HomepageController.getHomepageView)
     .get("/menu")
-    .get("/reservation")
+    .get("/reservation", HomepageController.getHomepageReservationView)
     .post("/reservation")
 
 export default indexRoute
