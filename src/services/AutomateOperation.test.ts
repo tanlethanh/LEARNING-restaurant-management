379db6194a302d07906be25c949b8eac2679b3ab
Tan Le <tanlethanh8266@gmail.com>
2022-12-17 09:54:39 +0700

    update ran

--- src/services/AutomateOperation.test.ts
+++ src/services/AutomateOperation.test.ts
@@ -17,7 +17,7 @@ export async function createScenario(callback: Function) {
 
     dropReservationToday()
     const timeSlot = await generateReservation(
-        20,
+        70,
         Locals.config().automateDurationInSeconds * 2,
         Locals.config().automateDurationInSeconds
     )
